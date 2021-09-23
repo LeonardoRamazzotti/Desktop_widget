61088d4e4aaf80c59cd544cdfc565b227a8ae2af
LeonardoRamazzotti <leonardo.ramazzotti95@gmail.com>
2021-09-23 23:26:42 +0200

    image and main update

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from tkinter import *
 
 root = Tk()
 root.title('Essential Widget')
-root.geometry('400x300')
+root.geometry('400x300+50+50')
+root.overrideredirect(True)
+root.wm_attributes('-transparentcolor','red')
+root.config(bg='red')
 
 
+image_bg= PhotoImage(file='bg_main.png')
 
+
+Label_bg = Label(root, image = image_bg,bg='red')
+
+Label_bg.pack()
+
+root.bind('a', lambda event: root.destroy())
 
 
 root.mainloop()
