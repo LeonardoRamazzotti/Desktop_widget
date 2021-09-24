8567ba630ef415194e580ef06536d5bc8ec6db54
LeonardoRamazzotti <leonardo.ramazzotti95@gmail.com>
2021-09-24 09:11:42 +0200

    main update

--- main.py
+++ main.py
@@ -48,26 +48,34 @@ def weather(link,head):   # funzione che fa web scraping e cerca meteo nella pos
         weather_img = weather_cloudy
     
     Label_Weather = Label(root, image = weather_img,bg='#33363B')
-    Label_Weather.place(x=40,y=85)    
+    Label_Weather.place(x=40,y=95)    
 
     Label_luogo = Label(root, text=Luogo,font = ('ABeeZee',13),bg='#33363B', fg= 'white' )
-    Label_luogo.place(x=40,y=165) 
+    Label_luogo.place(x=40,y=185) 
 
-    Label_wind = Label(root,text='Wind:'+wind_speed,font = ('ABeeZee',12),bg='#333B41', fg= 'white' )
-    Label_wind.place(x=110,y=110)
+    Label_wind = Label(root,text='Wind:'+wind_speed,font = ('ABeeZee',12),bg='#33363B', fg= 'white' )
+    Label_wind.place(x=115,y=140)
     
-    Label_rainfall = Label(root,text='Raifall:'+rainfall,font = ('ABeeZee',12),bg='#333B41', fg= 'white')
-    Label_rainfall.place(x=110,y=130)
+    Label_rainfall = Label(root,text='Raifall:'+rainfall,font = ('ABeeZee',12),bg='#33363B', fg= 'white')
+    Label_rainfall.place(x=115,y=160)
 
-    Label_temp = Label(root,text=temp+'°C',font = ('ABeeZee',28),bg='#202329', fg= '#757778')
-    Label_temp.place(x=220,y=19)
-
-
-    Label_cond =Label(root, text=condizione,font = ('ABeeZee',12),bg='#333B41', fg= '#757778' )
-    Label_cond.place(x=220,y=322)
+    Label_temp = Label(root,text=temp+'°C',font = ('ABeeZee',20),bg='#33363B', fg= 'white')
+    Label_temp.place(x=115,y=90)
 
 
+    Label_cond =Label(root, text=condizione,font = ('ABeeZee',12),bg='#33363B', fg= 'white' )
+    Label_cond.place(x=110,y=120)
 
+
+def time_now(link,head):
+    
+    page = requests.get(link, headers=head)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    curr_time = soup.find_all("div", {"class": "gsrt vk_bk FzvWSb XcVN5d YwPhnf"})
+   
+    label_curr_time = Label(root, text=curr_time,font = ('ABeeZee',12),bg='#33363B', fg= 'white')
+    label_curr_time.place(x=160,y=90)
 
 
 root = Tk()
@@ -85,7 +93,7 @@ else:
     fonts = ("Courier", 12)
     border='black'
 
-image_bg= PhotoImage(file='bg_main.png')
+image_bg= PhotoImage(file='Web 1280 – 1.png')
 weather_sunny = PhotoImage(file='sunny.png')
 weather_cloudy = PhotoImage(file='cover.png')
 weather_rainy = PhotoImage(file='rain.png')
@@ -114,11 +122,15 @@ headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 URL = 'https://www.google.com/search?q=weather+location&oq=weather+location&aqs=chrome..69i57.3274j0j7&sourceid=chrome&ie=UTF-8'
 
+URL_time = 'https://www.google.com/search?q=ora&oq=ora&aqs=chrome..69i57j69i61l2.2138j0j7&sourceid=chrome&ie=UTF-8'
+
 weather(URL,headers)
+time_now(URL_time,headers)
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 root.bind('a', lambda event: root.destroy())
+root.bind('Ctrl+Shift+q', lambda event: root.destroy())
 
 
 root.mainloop()
